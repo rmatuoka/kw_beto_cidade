d7c6d2357423cf76088e42ade191c7e5d994a7cc
Bruno Alves <bsouza@korewa.com.br>
2013-08-07 16:10:30 -0300

    Primeiro Push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwBetoCidades::Application.config.secret_token = 'a55dcca269e5f889e40626377dedd9b5b82da8c2f7db8cd7540b1b1fe666540ae3d24b663eae6b3bcd30d24985b75ead9a81ba513c7cc35dc087e76cf5ee55f7'
