a7b814ce5a0bf6c8be1720d17a23ea4d6651b565
-== Zaba ==- <bsilva@korewa.com.br>
2014-01-21 11:04:21 -0200

    Ajustes internos

--- app/models/being.rb
+++ app/models/being.rb
@@ -2,7 +2,7 @@ require 'custom_logger'
 
 class Being < ActiveRecord::Base
   #attr_accessible :name, :date_of_birth, :schooling_id, :city_id, :party_id, :being_type_id, :group_ids, :user_id
-  validates_presence_of :name #, :schooling_id, :city_id, :party_id
+  validates_presence_of :name, :being_type_id#, :schooling_id, :city_id, :party_id
   
   belongs_to :party
   belongs_to :city_being, :class_name => "City", :foreign_key => "city_id"
